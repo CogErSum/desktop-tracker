9427c7f234e0ecbed42593ed4a871b5f0942698c
cogersum <thehourjust@gmail.com>
2026-07-04 17:10:03 +0300

    feat: restore MenuBarExtra for menu bar mode

--- DesktopTracker/App/DesktopTrackerApp.swift
+++ DesktopTracker/App/DesktopTrackerApp.swift
@@ -3,7 +3,11 @@ import SwiftUI
 @main
 struct DesktopTrackerApp: App {
     var body: some Scene {
-        WindowGroup {
+        MenuBarExtra("TeamSight", systemImage: "timer") {
+            MenuBarView()
+        }
+        
+        WindowGroup("TeamSight Tracker") {
             ContentView()
         }
         .defaultSize(width: 1000, height: 700)
