bb91e9fab5f0dfb33a6d955ab490c049cbefc5c9
cogersum <thehourjust@gmail.com>
2026-07-04 17:41:01 +0300

    feat: auto-show main window on app launch

--- DesktopTracker/App/DesktopTrackerApp.swift
+++ DesktopTracker/App/DesktopTrackerApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct DesktopTrackerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         MenuBarExtra("TeamSight", systemImage: "timer") {
             MenuBarView()
@@ -11,5 +13,22 @@ struct DesktopTrackerApp: App {
             ContentView()
         }
         .defaultSize(width: 1000, height: 700)
+        .commands {
+            CommandGroup(replacing: .newItem) { }
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            for window in NSApplication.shared.windows {
+                if window.title == "TeamSight Tracker" {
+                    window.makeKeyAndOrderFront(nil)
+                    NSApp.activate(ignoringOtherApps: true)
+                    break
+                }
+            }
+        }
     }
 }
